a1dba2299573282c74ccae338dbcba4d6bf21875
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2022-11-21 17:01:14 -0300

    Change to  js modules

--- js/index.js
+++ js/index.js
@@ -1,28 +1,11 @@
-const routes = {
-  '/': '/pages/home.html',
-  '/universe': '/pages/universe.html',
-  '/exploration': '/pages/exploration.html',
-  404: '/pages/404.html'
-}
+import { Router } from './router.js'
 
-function route(event) {
-  event = event || window.event
-  event.preventDefault()
+const router = new Router()
+router.add('/', '/pages/home.html')
+router.add('/universe', '/pages/universe.html')
+router.add('/exploration', '/pages/exploration.html')
+router.add('404', '/pages/404.html')
 
-  window.history.pushState({}, '', event.target.href)
-
-  handle()
-}
-
-function handle() {
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-    .then(data => data.text())
-    .then(html => (document.querySelector('#app').innerHTML = html))
-}
-
-handle()
-window.onpopstate = () => handle()
-window.route = () => route()
+router.handle()
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
